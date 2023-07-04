57bb52219b45e18737e96c56ae6c09856a09caa8
XiaoLingH <xhuang0626@gmail.com>
2023-07-03 23:46:18 -0400

    Added a route to serve up a single campus based on its id

--- api/campuses.js
+++ api/campuses.js
@@ -27,4 +27,15 @@ router.post("/", async (req, res, next) => {
   }
 });
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    const campus = await Campus.findByPk(req.params.id);
+    campus
+      ? res.status(200).json(campus)
+      : res.status(404).send("Campus Not Found");
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
