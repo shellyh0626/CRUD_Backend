116d51c9f5d3bc94284c75eb8a0178e1f88e2d51
XiaoLingH <xhuang0626@gmail.com>
2023-07-03 22:48:40 -0400

    Added id to campus model

--- db/models/campus.js
+++ db/models/campus.js
@@ -2,6 +2,11 @@ const { DataTypes } = require("sequelize");
 const db = require("../db");
 
 const Campus = db.define("campus", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
   name: {
     type: DataTypes.STRING,
     allowNull: false,
