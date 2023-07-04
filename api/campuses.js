f15a0eef02ab712bcca82773b54be26bdb96b4e4
XiaoLingH <xhuang0626@gmail.com>
2023-07-04 16:32:22 -0400

    Added update campus information

--- api/campuses.js
+++ api/campuses.js
@@ -38,4 +38,16 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
+router.put("/:id", async (req, res, next) => {
+  try {
+    const campus = await Campus.findByPk(req.params.id);
+    campus
+      ? await campus.update(req.body)
+      : res.status(404).send("Campus Not Found");
+    res.status(200).json(campus);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
