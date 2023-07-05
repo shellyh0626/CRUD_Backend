88b58b336c20d3460ae5ab12f2d44fa7975364fa
XiaoLingH <xhuang0626@gmail.com>
2023-07-05 16:15:12 -0400

    Added cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+var cors = require("cors");
+
 const db = require("./db");
 const PORT = 8080;
 
 const app = express();
+app.use(cors());
 app.use("/api", require("./api"));
 
 const syncDB = () => db.sync();
